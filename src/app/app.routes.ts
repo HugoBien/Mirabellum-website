d0fa7c76a8adc5a7d96a3e03a5600da30b6aa206
Hugo <hug.bien@gmail.com>
2026-04-19 22:46:16 +0200

    add header bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/page/home/home';
+import { Contact } from './component/page/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'contact',
+        component: Contact
+    }
+];
